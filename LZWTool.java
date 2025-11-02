cfe8e6af294776796d6df9c95c35e80d71b7f6fd
园田海未 <18519644256@163.com>
2025-11-01 23:31:10 -0400

    1st

--- LZWTool.java
+++ LZWTool.java
@@ -53,7 +53,7 @@ public class LZWTool
                     maxW = Integer.parseInt(args[++i]);
                     break;
                 case "--policy":
-                    policy = "freeze";
+                    policy = args[++i];
                     break;
                 case "--alphabet":
                     alphabetPath = args[++i];
